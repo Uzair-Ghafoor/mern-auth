ef032b5876660ea894d7a6db8226086f4d05e44c
Uzair-Ghafoor <uzairgbgrt@gmail.com>
2024-07-10 03:37:27 +0500

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
-
-const App = () => {};
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
+import { Profile, About, Signin, Signup, Home } from './pages';
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as About } from './About';
+export { default as Profile } from './Profile';
+
+export { default as Signin } from './Signin';
+export { default as Signup } from './Signup';
+export { default as Home } from './Home';
